2f027a2abe55a46b5c67e0d4b1b10f438e11166e
sebasti006 <sebastianbriones006@gmail.com>
2026-03-18 10:29:50 -0500

    Nuevo Proyecto

---
+++ src/main/java/ec/edu/espoch/nuevap/NuevaP.java
@@ -0,0 +1,76 @@
+package ec.edu.espoch.nuevap;
+
+import java.util.Scanner;
+
+public class NuevaP {
+    public static void main(String[] args) {
+    
+   /* byte numUno=6;
+    byte numDos=8;
+    if(numUno>=0 && numUno<=10){
+        if(numDos>=0 && numDos<=10){
+            if(numUno > numDos){
+                System.out.println("el Numero mayor es "+ numUno);
+            }else{ 
+                System.out.println("El numero mayor es "+ numDos);
+             
+            }
+        }
+    }
+        int opcion=4;
+        int numUno=6;
+        int numDos=3;
+        int resultado=0;
+        switch (opcion) {
+            case 1:
+                resultado= numUno+numDos;
+                System.out.println("Resultado es "+resultado);
+                break;
+            case 2:
+                resultado= numUno-numDos;
+                System.out.println("Resultado es  "+ resultado);
+                break;
+                case 3:
+                resultado= numUno*numDos;
+                System.out.println("Resultado es  "+ resultado);
+                break;
+                case 4:
+                resultado= numUno/numDos;
+                if(numDos>0){
+                System.out.println("Resultado es  "+ resultado);
+                }
+                break;
+                
+            default:
+                System.out.println("Error ");;
+        }
+    
+            int multi = 20;
+           for (int i = 0; i < 10; i=i+1) {
+               System.out.println(multi+" x "+i+"= "+(multi*i));
+               
+               
+        }
+*/
+           int contador =1;
+           int multi=5;
+      /*     while (contador <100) { 
+               //se va a repetir 
+               
+                   System.out.println(multi+" x "+ contador +"= "+ (multi*contador));
+               contador =contador +1;   
+               }
+    */
+           do {            
+            System.out.println(multi+" x "+ contador +"= "+ (multi*contador));
+               contador =contador +1; 
+        } while (contador <1);
+                  
+        }
+      
+    
+}
+
+
+    
+
